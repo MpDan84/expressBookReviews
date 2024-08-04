43eea3ccfb82165817b12a33f1495ffe6a7df0cd
MpDan84 <mpdaniel84@hotmail.com>
2024-08-04 15:12:53 +0100

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart","ISBN":"0001", "reviews": {"review1":"Like a bad marriage"} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales","ISBN":"0002", "reviews": {"review2":"It's not real"} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy","ISBN":"0003", "reviews": {"review3":"Not that funny"} },
+      4: {"author": "Ancient Mesopotamia","title": "The Epic Of Gilgamesh","ISBN":"0004", "reviews": {"review4":"Dead language"} },
+      5: {"author": "Bible","title": "The Book Of Job","ISBN":"0005", "reviews": {"review5":"Goes on a bit"} },
+      6: {"author": "Genie of the Lamp","title": "One Thousand and One Nights","ISBN":"0006", "reviews": {"review6":"You ain't never hada friend like me"} },
+      7: {"author": "Thor Odinson","title": "Nj\u00e1l's Saga","ISBN":"0007", "reviews": {"review7":"He whoever hold this hammer shall be worthy"} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice","ISBN":"0008", "reviews": {"review8":"Oh, Mr Darcy!"} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot","ISBN":"0009", "reviews": {"review9":"Bonjour Monsiuer"} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy","ISBN":"0010", "reviews": {"review10":"Killer Ending"} }
 }
 
 module.exports=books;
